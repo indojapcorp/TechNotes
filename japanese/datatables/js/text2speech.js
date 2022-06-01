963f2ee0ff94848355cad82819c94591078ac96b
Indo Jap <indojapcorp@gmail.com>
2022-06-01 16:43:05 +0530

    Japanese datatables

--- japanese/datatables/js/text2speech.js
+++ japanese/datatables/js/text2speech.js
@@ -1,39 +1,25 @@
 let speech = new SpeechSynthesisUtterance();
 speech.lang = "en";
+speech.rate = 1;
+speech.volume = 1;
+speech.pitch = 1;
 
+  
 let voices = [];
-window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices();
-  speech.voice = voices[0];
-  let voiceSelect = document.querySelector("#voices");
-  voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
-};
 
-document.querySelector("#rate").addEventListener("input", () => {
-  const rate = document.querySelector("#rate").value;
-  speech.rate = rate;
-  document.querySelector("#rate-label").innerHTML = rate;
-});
 
-document.querySelector("#volume").addEventListener("input", () => {
-  const volume = document.querySelector("#volume").value;
-  speech.volume = volume;
-  document.querySelector("#volume-label").innerHTML = volume;
-});
-
-document.querySelector("#pitch").addEventListener("input", () => {
-  const pitch = document.querySelector("#pitch").value;
-  speech.pitch = pitch;
-  document.querySelector("#pitch-label").innerHTML = pitch;
-});
-
-document.querySelector("#voices").addEventListener("change", () => {
-  speech.voice = voices[document.querySelector("#voices").value];
-});
 
 document.querySelector("#start").addEventListener("click", () => {
   //speech.text = document.querySelector("textarea").value;
   speech.text = window.getSelection().toString();
+  speakVoice(0);
+  window.speechSynthesis.speak(speech);
+});
+
+document.querySelector("#jap").addEventListener("click", () => {
+  //speech.text = document.querySelector("textarea").value;
+  speech.text = window.getSelection().toString();
+  speakVoice(18);
   window.speechSynthesis.speak(speech);
 });
 
@@ -48,3 +34,9 @@ document.querySelector("#resume").addEventListener("click", () => {
 document.querySelector("#cancel").addEventListener("click", () => {
   window.speechSynthesis.cancel();
 });
+
+
+function speakVoice(num) {
+voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[num];
+};
