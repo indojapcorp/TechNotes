bb596b709171f59eeee47b54aede73fe9e5304ea
Indo Jap <indojapcorp@gmail.com>
2022-06-17 18:20:03 +0530

    pause resume stop loop play

--- japanese/datatables/js/text2speech.js
+++ japanese/datatables/js/text2speech.js
@@ -21,11 +21,15 @@ var selectedText;
 var pagelang;
 var fclang;
 var sclang;
+var everyline;
+var times;
+play=false;
 
 document.onselectionchange = userSelectionChanged;
 
 function userSelectionChanged() {
 
+	play=true;
     // wait 500 ms after the last selection change event
     if (selectionEndTimeout) {
         clearTimeout(selectionEndTimeout);
@@ -37,12 +41,10 @@ function userSelectionChanged() {
     }, 500);
 }
 
+
 $(window).bind('selectionEnd', function () {
 
     selectionEndTimeout = null;
-
-    // TODO: Do your cool stuff here........
-
     selectedText = window.getSelection().toString();
     if(selectedText != ''){
 speech.text = selectedText;
@@ -50,6 +52,8 @@ speech.text = selectedText;
 fclang=$( "#fclang" ).val();
 sclang=$( "#sclang" ).val();
 
+times=$( "#times" ).val();
+everyline=$( "#everyline" ).val();
 
 
 pagelang=$( "html" ).attr("lang");
@@ -82,7 +86,7 @@ var length =selectedText.split(/\r?\n/).length;
 //}
 
 //speakMessage(selectedText,500,pagelang)
-speakTwoMessages(selectedText,500);
+speakTwoMessages(selectedText,500,times,everyline);
 
 //  window.speechSynthesis.speak(speech);    
   }else{
@@ -128,9 +132,36 @@ function speakMessage(message, PAUSE_MS = 500,lang) {
 }
 
 
-function speakTwoMessages(message, PAUSE_MS = 500) {
+function speakTwoMessages(message, PAUSE_MS = 500 , TIMES = 1 , EVERYLINE = 0) {
   try {
-    const messageParts = message.split(/\r?\n/)
+  	
+  	console.log('times='+times);
+  	console.log('EVERYLINE='+EVERYLINE);
+  	  	
+  	var sb = "";
+  	var sbfull = "";
+  	var sbeachline = "";  	  	
+
+	
+	if(EVERYLINE==1){
+		const messageLineParts = message.split(/\r?\n/)
+		for (let i = 0; i < messageLineParts.length; i++) {
+  			sbeachline = sbeachline + messageLineParts[i] + '\n' + messageLineParts[i] + '\n'  ;
+		}
+		sb=sbeachline;
+	}else{
+	  	for (let i = 0; i < TIMES; i++) {
+  		sbfull = sbfull + message + '\n' ;
+		}
+		sb=sbfull;
+	}
+	
+	//console.log(sbeachline);
+
+	console.log(sb);
+
+
+    const messageParts = sb.split(/\r?\n/)
 
     let currentIndex = 0
     const speak = (textToSpeak1,textToSpeak2) => {
@@ -164,8 +195,10 @@ function speakTwoMessages(message, PAUSE_MS = 500) {
         currentIndex++;
         if (currentIndex < messageParts.length) {
           setTimeout(() => {
+          if(play){
             const messagetabParts = messageParts[currentIndex].split(/\t/);
             speak(messagetabParts[0],messagetabParts[1])
+            }
           }, PAUSE_MS);
         }
       };
@@ -189,3 +222,53 @@ function speakTwoMessages(message, PAUSE_MS = 500) {
 function run(pause) {
   speakMessage('Testing 1,2,3', pause)
 }
+
+try {
+document.querySelector("#pause").addEventListener("click", () => {
+  window.speechSynthesis.pause();
+});
+
+document.querySelector("#resume").addEventListener("click", () => {
+  window.speechSynthesis.resume();
+});
+
+document.querySelector("#cancel").addEventListener("click", () => {
+  play=false;
+  window.speechSynthesis.cancel();
+});
+}
+catch(err) {
+  console.log(' pause/resume button not found');
+}
+
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    console.log('file:'+file);
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+
